48e7bf7181e2865bbb313e1d8ac97370efd02e0d
ANIRUDH P S <anirudhpskumar2004@gmail.com>
2026-09-12 19:37:25 +0530

    Update palindrome12.c

--- palindrome12.c
+++ palindrome12.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
 
+int main()
+{
+    char str[100], rev[100];
+    int i = 0, len = 0, same = 1;
+
+    printf("Enter a document: ");
+    scanf("%s", str);
+
+    // Find length manually 
+    while (str[len] != '\0')
+        len++;
+
+    // Reverse manually 
+    for (i = 0; i < len; i++)
+        rev[i] = str[len - i - 1];
+
+    rev[len] = '\0';
+
+    // Compare manually 
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] != rev[i])
+        {
+            same = 0;
+            break;
+        }
+    }
+
+    printf("Reversed document: %s\n", rev);
+
+    if (same)
+        printf("The document is a palindrome.\n");
+    else
+        printf("The document is not a palindrome.\n");
+
+    return 0;
+}
