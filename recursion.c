b9f43926406988d72219379cf58ee5093ae07c65
ANIRUDH P S <anirudhpskumar2004@gmail.com>
2026-09-13 11:53:27 +0530

    Create recursion.c

---
+++ recursion.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverseWords(char str[], int start, int end)
+{
+    int i;
+
+    // Find the end of the current word
+    for (i = start; i < end && str[i] != ' '; i++);
+
+    if (i == end)
+    {
+        printf("%.*s", end - start, str + start);
+        return;
+    }
+
+    // Recursively process the remaining words
+    reverseWords(str, i + 1, end);
+
+    printf(" %.*s", i - start, str + start);
+}
+
+int main()
+{
+    char str[200];
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("Reversed sentence: ");
+    reverseWords(str, 0, strlen(str));
+
+    return 0;
+}
