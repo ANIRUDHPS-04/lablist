fe76f8fdd97c475dfab5a0e9f46a35c51aaa29ab
ANIRUDH P S <anirudhpskumar2004@gmail.com>
2026-09-14 19:08:38 +0530

    Update char.c

--- char.c
+++ char.c
@@ -1,41 +1,48 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 int main()
 {
-char str[100];
-int alpha=0,digit=0,punct=0;
-printf("Enter the String:");
-fgets(str,100,stdin);
-printf("Character Breakdown:<br>");
+    char str[100];
+    int vowel = 0, consonant = 0, digit = 0, punct = 0;
 
-for(int i=0;i<strlen(str);i++)
-{
-  char c = tolowerstr[i];
+    printf("Enter the String: ");
+    fgets(str, 100, stdin);
 
-  if(isalpha(str[i]))
-  {
-    switch(c){
-   case 'a':
-   case 'e':
-   case 'i':
-   case 'o':
-   case 'u':
-     break;
-    alpha++;
-  } 
-  else if(isdigit(str[i]))
-  {
-    printf("Consonanat");
-    digit++;
-  } else if(ispunct(str[i])){
-    printf("Others");
-    punct++;
-  }
+    printf("\nCharacter Breakdown:\n");
 
-  printf("<br>");
-}
-printf("\nSummary: %d Alphabets, %d Consonanat, %d Others<br>",alpha,digit,punct);
+    for(int i = 0; i < strlen(str); i++)
+    {
+        char c = tolower(str[i]);
 
-return 0;
+        if(isalpha(str[i]))
+        {
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            {
+                printf("%c - Vowel\n", str[i]);
+                vowel++;
+            }
+            else
+            {
+                printf("%c - Consonant\n", str[i]);
+                consonant++;
+            }
+        }
+        else if(isdigit(str[i]))
+        {
+            printf("%c - Digit\n", str[i]);
+            digit++;
+        }
+        else if(ispunct(str[i]))
+        {
+            printf("%c - Others\n", str[i]);
+            punct++;
+        }
+    }
+
+    printf("\nSummary: %d Vowels, %d Consonants, %d Digits, %d Others\n",
+           vowel, consonant, digit, punct);
+
+    return 0;
 }
